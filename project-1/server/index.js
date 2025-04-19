fa90dc9e7bd5592cab000f2e5981d89f277447fe
VishalChoudhary <vishalghn953@gmail.com>
2025-04-19 16:57:32 +0530

    api route testing

--- project-1/server/index.js
+++ project-1/server/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const userRouter = require('./routes/user');
 
 require('./db');
+
+app.use(express.json());
+app.use("/api/users",userRouter);
 
 app.get("/",(req,res)=>{
     res.send("Welcome to port 5000");
