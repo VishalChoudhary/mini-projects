6cbcb78aa2a972d99fa8892fb7cc29f80b4f5d4b
VishalChoudhary <vishalghn953@gmail.com>
2025-04-29 16:07:04 +0530

    cors commit

--- project-1/server/index.js
+++ project-1/server/index.js
@@ -3,10 +3,13 @@ const express = require("express");
 const app = express();
 const userRouter = require('./routes/user');
 const authRouter = require('./routes/auth');
+const cors = require('cors');
 
 require('./db');
 
 app.use(express.json());
+app.use(cors({origin: 'http://localhost:3000'}));
+
 app.use("/api/users",userRouter);
 app.use("/api/auth",authRouter);
 
